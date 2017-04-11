f4c993c16914a6f21dc3d95d552c9ebe3ebd3e3a
Brandon Myers <brandon-d-myers@uiowa.edu>
2017-04-11 09:43:30 -0500

    an empty Solver.java

---
+++ src/Solver.java
@@ -0,0 +1,8 @@
+public class Solver {
+	public static void main(String[] args) {
+		// args[0] is the filename of the initial puzzle configuration
+		// args[1] is the filename of the goal
+   
+	}
+	
+}
