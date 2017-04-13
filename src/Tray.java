841c78e68ae7f5002a6f2ea79f265eb1f724077f
madsilva <madsilva@172.17.122.68>
2017-04-13 18:34:26 -0500

    hej

--- src/Tray.java
+++ src/Tray.java
@@ -9,7 +9,19 @@
  * @author madsilva
  */
 public class Tray {
-    public Tray() {
+    private int width;
+    private int height;
+    private int[][] tray;
+    
+    public Tray(int w, int h) {
+        width = w;
+        height = h;
+        tray = new int[w][h];
+       
+    }
+    
+    // takes a line of input representing a block and adds it to the tray
+    public void addBlock(String line) {
         
     }
 }
