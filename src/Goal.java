4dd14504cebd830f9cdb9d2d135dbb826f7a0fd3
madsilva <madsilva@172.17.122.68>
2017-04-16 00:56:36 -0500

    very small changes

---
+++ src/Goal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author madsilva
+ */
+public class Goal extends Tray {
+    // Each Goal has a tray array
+    // how to compare goal tray to game tray?
+    // if goal tray matches up to game tray, then proceed to next step
+    // go thru all blocks in goal and check if they exist in the game tray
+    // if they all do then check returns true
+    
+    public Goal() {
+        super(0, 0);
+    }
+    
+    // need to add goal instance variable to tray
+    
+    public boolean check(Tray game) {
+        
+    }
+}
