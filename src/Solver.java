5ced380d1776b1de013940e46ba27512a12ed252
jgeati <jgeati@dhcp80ff4f02.dynamic.uiowa.edu>
2017-05-01 13:03:16 -0500

    Added way to take optional arguments, notably -h

--- src/Solver.java
+++ src/Solver.java
@@ -2,18 +2,44 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solver {
     // args[0] is the filename of the initial puzzle configuration
     // args[1] is the filename of the goal	
     public static void main(String[] args) {
         
-        // implement option debugging messages
+        
+        String[] oarguments = null;
+        String[] arguments = null;
+        for (String arg : args){
+            // help menu if the first argument is help, exits program, assumes -h will be only argument
+            if (arg.equals("-h")){
+                System.out.println("No options implemented (yet)");
+                System.exit(0);
+            }
+            // finds main arguments
+            if (!arg.substring(0, 1).equals("-")){
+                // separates arguments into separate arrays
+                if (!arg.equals(args[0]))
+                    oarguments = Arrays.copyOfRange(args, 0, Arrays.asList(args).indexOf(arg));
+                arguments = Arrays.copyOfRange(args, Arrays.asList(args).indexOf(arg), args.length);
+                // stops looping
+                break;
+            }
+        }
+        
+        // prints all optional arguments for debugging purposes
+        if (oarguments != null){
+            for (String arg : oarguments){
+                System.out.println(arg);
+            }
+        }
         
         try {
             
             // Intializing the game tray
-            BufferedReader in1 = new BufferedReader(new FileReader(args[0]));
+            BufferedReader in1 = new BufferedReader(new FileReader(arguments[0]));
             String line = in1.readLine();
             String[] lineVals = line.split(" ");
             Tray game = new Tray(Integer.parseInt(lineVals[0]), Integer.parseInt(lineVals[1]));
@@ -25,7 +51,7 @@ public class Solver {
             in1.close();
             // Intializing the goal tray
             Tray goal = new Tray(game.getRows(), game.getCols());
-            BufferedReader in2 = new BufferedReader(new FileReader(args[1]));
+            BufferedReader in2 = new BufferedReader(new FileReader(arguments[1]));
             line = in2.readLine();
             while (line != null) {
                 goal.addBlock(line);
@@ -56,7 +82,7 @@ public class Solver {
             }
         }
         catch (IOException e) {
-            System.out.println("File could not be opened.");
+            System.out.println("Invalid Argument");
         }  
     }	
 }
